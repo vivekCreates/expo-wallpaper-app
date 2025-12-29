15869ec519f10628b96984840197416bb825da8b
vivek <codecrafter67@gmail.com>
2025-12-29 19:02:28 +0530

    refactor: restructure layout and add tab navigation; restore account, explore, and for you screens

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,30 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue'  }} >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'For You',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+        <Tabs.Screen
+          name="explore"
+          options={{
+            title: 'Explore',
+            tabBarIcon: ({ color }) => <FontAwesome size={28} name="compass" color={color} />,
+          }}
+        />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: 'Account',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,8 @@
-import { Link, Slot } from "expo-router";
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Stack } from "expo-router";
 
-export default function Layout() {
-    return(
-    <SafeAreaView>
-        <View style={{ width: "100%", height: "97%" }}>
-            <Slot />
-        </View>
-        <View style={{ flexDirection: "row", paddingHorizontal: 10, justifyContent: "space-between", backgroundColor: "blue" }}>
+export default function Layout(){
 
-            <Link href={"/"}><Text>For you</Text></Link>
-            <Link href={"/explore"}><Text>Explore</Text></Link>
-            <Link href={"/account"}><Text>Account</Text></Link>
-        </View>
-    </SafeAreaView>)
+  return <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
 }
